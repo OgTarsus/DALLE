ed5aa0d4ebcf1ef078eb3f8cbf3c74631e15e42e
OgTarsus <centrocimah6070@gmail.com>
2026-08-12 19:46:15 +0100

    configure production API URL

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -23,7 +23,7 @@ const CreatePost = () => {
       setLoading(true)
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/post`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -61,7 +61,7 @@ const CreatePost = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/dalle`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
